3e0bfc740a2fc3089c6530f29f5147100c120a08
ncuasput <nm.cuaspudt@uea.edu.ec>
2026-01-09 20:14:40 -0500

    Tarea semana 5

--- SEMANA 5/script.js
+++ SEMANA 5/script.js
@@ -1,6 +1,17 @@
-function cambiaTexto() {
-    document.getElementById("texto").innerHTML = "¡Has cambiado el texto!";
-}
-function cambiaColor() {
-    document.getElementById("texto").style.backgroundColor="lightblue";
+// Traer los elementos del DOM.
+const inputUrl = document.getElementById("inputUrl");
+const botonCargar = document.getElementById("btnAgregar");
+const imagen = document.getElementById("imagen");
+
+let imagenSeleccionada = null;
+// Función para agregar un anueva imagen 
+function agregarImagen()
+const url = inputUrl.value;
+if (url) {
+    const nuevaImagen = document.createElement("img");
+    nuevaImagen.src= url;
+    nuevaImagen.classList.add("imagen-galería");
+    nuevaImagen.addEventListener("click", () => seleccionarImagen);
+    galeria.appendChild(nuevaImagen);
+    inputUrl.value = "";
 }
