225c0203a508733b8c42292ef050792d8c7bd856
ncuasput <nm.cuaspudt@uea.edu.ec>
2026-01-23 19:39:51 -0500

    Deber

--- SEMANA 7/script.js
+++ SEMANA 7/script.js
@@ -3,9 +3,9 @@ const productlist = document.getElementById("productlist");
 const btnAgregar = document.getElementById("btnAgregar");
 // Arreglo que almacenará los productos
 const productos = [
-    { nombre: "Tablet", precio: "$200", descripcion: "Dispositivo electrónico portátil con una pantalla táctil grande" },
-    { nombre: "Laptop", precio: "$600", descripcion: "Contiene teclado, panel táctil (touchpad), altavoces " },
-    { nombre: "Auriculares", precio: "$30", descripcion: "Reciben señales eléctricas de un equipo de audio" }
+    { nombre: "Tablet", precio: "$200", descripcion: "Dispositivo electrónico portátil con una pantalla táctil grande." },
+    { nombre: "Laptop", precio: "$600", descripcion: "Contiene teclado, panel táctil (touchpad), altavoces. " },
+    { nombre: "Auriculares", precio: "$30", descripcion: "Reciben señales eléctricas de un equipo de audio." }
 ];
 
 
@@ -35,7 +35,7 @@ function agregarProducto() {
     const nuevoProducto = {
         nombre: "Parlantes",
         precio: "$700",
-        descripcion: "Dispositivo electroacústico"
+        descripcion: "Dispositivo electroacústico."
     };
 
     // Agrega el nuevo producto al arreglo
