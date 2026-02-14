9a891b2ee7de71c736b4a024595b3f023adf7e15
ncuasput <nm.cuaspudt@uea.edu.ec>
2026-02-14 13:59:34 -0500

    Avance

--- PROYECTO/venv/app.py
+++ PROYECTO/venv/app.py
@@ -5,9 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '¡Hola, mundo!'
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
-ghghg
+# ruta con parametro
+@app.route(*/usuario/<nombre>'')
+def  usuario(nombre):
+    return f'Bienvenido estas en clase, { nombre}!'
+# ruta con contacto
+@app.route('*/contacto')
+def  contacto():
+    return '../template/vistas/contacto.html'
 
 
