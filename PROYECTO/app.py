ad5c73fe5055e0d9ffb4c7f88ed32cdc8bfb5c43
ncuasput <nm.cuaspudt@uea.edu.ec>
2026-02-14 14:45:54 -0500

    Tarea

--- PROYECTO/app.py
+++ PROYECTO/app.py
@@ -5,18 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '¡Hola, mundo!'
-    
 
+# Ruta con parámetro
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Hola, {nombre}!'
 
-# ruta con parametro
-@app.route(*/usuario/<nombre>'')
-def  usuario(nombre):
-    return f'Bienvenido estas en clase, { nombre}!'
-# ruta con contacto
-@app.route('*/contacto')
-def  contacto():
+# Ruta con contacto
+@app.route('/contacto')
+def contacto():
     return '../template/vistas/contacto.html'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
